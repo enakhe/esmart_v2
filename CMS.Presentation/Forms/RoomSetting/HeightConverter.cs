7f12e9a4d11c1f94a3b009564852f15d05333db5
ENAKHE <izuagbesam@gmail.com>
2025-04-12 12:34:16 +0100

    Fixed floating and responsive design for the room settings page

---
+++ CMS.Presentation/Forms/RoomSetting/HeightConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ESMART.Presentation.Forms.RoomSetting
+{
+    public class HeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double originalHeight = (double)value;
+            double offset = 150;
+
+            return Math.Max(0, originalHeight - offset);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CMS.Presentation/Forms/RoomSetting/RoomStatusToColorDialog.cs
+++ CMS.Presentation/Forms/RoomSetting/RoomStatusToColorDialog.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace ESMART.Presentation.Forms.RoomSetting.Room
+namespace ESMART.Presentation.Forms.RoomSetting
 {
     public class RoomStatusToColorDialog : IValueConverter
     {
